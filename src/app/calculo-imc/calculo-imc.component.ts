7afb172947d2a33c8c04fd7170e28638a55547f3
SorayaYF <sorayayasin@hotmail.com>
2023-07-06 14:21:27 -0300

    RadioButton Sexo, Avaliação e IconeAvaliação

--- src/app/calculo-imc/calculo-imc.component.ts
+++ src/app/calculo-imc/calculo-imc.component.ts
@@ -6,19 +6,43 @@ import { Component } from '@angular/core';
   styleUrls: ['./calculo-imc.component.scss']
 })
 export class CalculoImcComponent {
-
   public peso: number = 0;
   public altura: number = 0;
   public imc: number = 0;
+  public sexo: string = '';
+  public avaliacao: string = '';
 
   isButtonDisabled(): boolean {
-    return this.peso <= 0 || this.altura <= 0;
+    return this.peso <= 0 || this.altura <= 0 || this.sexo == '';
   }
-  
-  calcularIMC(){
 
+  calcularIMC() {
     this.imc = this.peso / (this.altura * this.altura);
 
+    if (this.sexo === "masculino") {
+      if (this.imc < 20.7) {
+        this.avaliacao = "Abaixo do peso";
+      } else if (this.imc < 26.4) {
+        this.avaliacao = "Peso normal";
+      } else if (this.imc < 27.8) {
+        this.avaliacao = "Marginalmente acima do peso";
+      } else if (this.imc < 31.1) {
+        this.avaliacao = "Acima do peso ideal";
+      } else {
+        this.avaliacao = "Obeso";
+      }
+    } else if (this.sexo === "feminino") {
+      if (this.imc < 19.1) {
+        this.avaliacao = "Abaixo do peso";
+      } else if (this.imc < 25.8) {
+        this.avaliacao = "Peso normal";
+      } else if (this.imc < 27.3) {
+        this.avaliacao = "Marginalmente acima do peso";
+      } else if (this.imc < 32.3) {
+        this.avaliacao = "Acima do peso ideal";
+      } else {
+        this.avaliacao = "Obeso";
+      }
+    }
   }
-
 }
